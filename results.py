d365254f9a9817e4ad181f183c22a0d1bd022d15
SAYYAD ABDUL LATIF <108394503+Sayyad-Abdul-latif@users.noreply.github.com>
2022-10-13 17:54:34 +0530

    Add files via upload

---
+++ results.py
@@ -0,0 +1,15 @@
+# importing requests
+import requests
+#import beautifulsoup
+from bs4 import BeautifulSoup
+#import pands
+import pandas as pd
+# get the content of website with requests
+r = requests.get("https://en.wikipedia.org/wiki/2019_Indian_general_election")
+# convert the request object into soup and parse it
+soup = BeautifulSoup(r.content, 'html.parser')
+# find all the tables in the dataframe
+table = soup.find_all("table")
+df = pd.read_html(str(table))
+# now convert selected dataframe into excel
+df[12].to_excel("results.xlsx")
